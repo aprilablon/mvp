437ba647d68219b9fac18e928733aab81c748f2d
April Ablon <ablonapril@gmail.com>
2017-04-28 21:12:38 -0700

    Added field names to give context to the info

--- client/src/components/LaunchListEntry.jsx
+++ client/src/components/LaunchListEntry.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 var LaunchListEntry = (props) => (
-  <div>
-    <p>I'm a launch list entry</p>
+  <div className="launch-list-entry">
+    <div className="name"><h3>{props.launch.name}</h3></div>
+    <div className="agency"><h4>{props.launch.agency}</h4></div>
+    <div className="start-time"><h5>When:</h5> {props.launch.starttime}</div>
+    <div className="location"><h5>Where:</h5> {props.launch.location}</div>
+    <div className="description"><h5>Why:</h5> {props.launch.description}</div>
+    <div className="video-url"><h5>Livestream:</h5> {props.launch.videourl}</div>
   </div>
 )
 
