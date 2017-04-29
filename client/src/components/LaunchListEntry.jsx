95f70d7d2c2f298ebe7d80744d303ecb58869c35
April Ablon <ablonapril@gmail.com>
2017-04-29 12:24:26 -0700

    Changed video url to embed video and displaying relative time

--- client/src/components/LaunchListEntry.jsx
+++ client/src/components/LaunchListEntry.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import Moment from 'react-moment';
 
 var LaunchListEntry = (props) => (
   <div className="launch-list-entry">
-    <div className="name"><h3>{props.launch.name}</h3></div>
-    <div className="agency"><h4>{props.launch.agency}</h4></div>
-    <div className="start-time"><h5>When:</h5> {props.launch.starttime}</div>
-    <div className="location"><h5>Where:</h5> {props.launch.location}</div>
-    <div className="description"><h5>Why:</h5> {props.launch.description}</div>
-    <div className="video-url"><h5>Livestream:</h5> {props.launch.videourl}</div>
+    <div className="name"><h3>{props.currentLaunch.name}</h3></div>
+    <div className="agency"><h4>{props.currentLaunch.agency}</h4></div>
+    <div className="start-time">
+      <h5>When:</h5>
+      <Moment fromNow>{props.currentLaunch.starttime}</Moment>
+    </div>
+    <Moment>{props.currentLaunch.starttime}</Moment>
+    <div className="location"><h5>Where:</h5> {props.currentLaunch.location}</div>
+    <div className="description"><h5>Why:</h5> {props.currentLaunch.description}</div>
+    <div className="video-url">
+      <h5>Livestream:</h5>
+      <iframe width="560" height="315" src={props.currentLaunch.embedurl} frameBorder="0" allowFullScreen></iframe>
+    </div>
   </div>
 )
 
