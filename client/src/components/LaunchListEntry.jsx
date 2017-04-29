afacb6b36d1303f54ae28261c59e83e09c6e55da
April Ablon <ablonapril@gmail.com>
2017-04-29 16:39:40 -0700

    Added Google Maps rendering for location

--- client/src/components/LaunchListEntry.jsx
+++ client/src/components/LaunchListEntry.jsx
@@ -4,16 +4,17 @@ import Moment from 'react-moment';
 var LaunchListEntry = (props) => (
   <div className="launch-list-entry">
     <div className="name"><h2>{props.currentLaunch.name}</h2></div>
-    <div className="agency"><h3>{props.currentLaunch.agency}</h3></div>
+    <div className="agency"><h2>{props.currentLaunch.agency}</h2></div>
     <div className="start-time">
-      <h4>When:</h4>
-      <Moment fromNow>{props.currentLaunch.starttime}</Moment>
+      <h4>When</h4>
+      <Moment fromNow id="relative-time">{props.currentLaunch.starttime}</Moment>
     </div>
     <Moment>{props.currentLaunch.starttime}</Moment>
-    <div className="location"><h4>Where:</h4> {props.currentLaunch.location}</div>
-    <div className="description"><h4>Why:</h4> {props.currentLaunch.description}</div>
+    <div className="location"><h4>Where</h4> {props.currentLaunch.location}</div>
+    <iframe width="560" height="315" frameBorder="0" src={props.currentLaunch.map} allowFullScreen></iframe>
+    <div className="description"><h4>Why</h4> {props.currentLaunch.description}</div>
     <div className="video-url">
-      <h4>Livestream:</h4>
+      <h4>Livestream</h4>
       <iframe width="560" height="315" src={props.currentLaunch.embedurl} frameBorder="0" allowFullScreen></iframe>
     </div>
   </div>
