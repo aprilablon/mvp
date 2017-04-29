40f9714170d865aee244bb8c4cc6829cdd1d3a8b
April Ablon <ablonapril@gmail.com>
2017-04-29 15:20:51 -0700

    Increased font size of headers

--- client/src/components/LaunchListEntry.jsx
+++ client/src/components/LaunchListEntry.jsx
@@ -3,17 +3,17 @@ import Moment from 'react-moment';
 
 var LaunchListEntry = (props) => (
   <div className="launch-list-entry">
-    <div className="name"><h3>{props.currentLaunch.name}</h3></div>
-    <div className="agency"><h4>{props.currentLaunch.agency}</h4></div>
+    <div className="name"><h2>{props.currentLaunch.name}</h2></div>
+    <div className="agency"><h3>{props.currentLaunch.agency}</h3></div>
     <div className="start-time">
-      <h5>When:</h5>
+      <h4>When:</h4>
       <Moment fromNow>{props.currentLaunch.starttime}</Moment>
     </div>
     <Moment>{props.currentLaunch.starttime}</Moment>
-    <div className="location"><h5>Where:</h5> {props.currentLaunch.location}</div>
-    <div className="description"><h5>Why:</h5> {props.currentLaunch.description}</div>
+    <div className="location"><h4>Where:</h4> {props.currentLaunch.location}</div>
+    <div className="description"><h4>Why:</h4> {props.currentLaunch.description}</div>
     <div className="video-url">
-      <h5>Livestream:</h5>
+      <h4>Livestream:</h4>
       <iframe width="560" height="315" src={props.currentLaunch.embedurl} frameBorder="0" allowFullScreen></iframe>
     </div>
   </div>
