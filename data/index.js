23f48c613bab7b483990dab039dad46f23821720
April Ablon <ablonapril@gmail.com>
2017-04-30 20:58:06 -0700

    Added favorites schema and collection

--- data/index.js
+++ data/index.js
@@ -15,6 +15,18 @@ var launches = new Schema({
   description: { type: String }
 })
 
-var Launches = mongoose.model('launches', launches);
+var favorites = new Schema({
+  name: { type: String, unique: true },
+  starttime: { type: Date },
+  embedurl: { type: String },
+  location: { type: String },
+  map: { type: String },
+  agency: { type: String },
+  description: { type: String }
+})
 
-module.exports = Launches;
+var Launches = mongoose.model('launches', launches);
+var Favorites = mongoose.model('favorites', favorites);
+
+module.exports.launches = Launches;
+module.exports.favorites = Favorites;
